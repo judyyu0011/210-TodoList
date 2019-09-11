3ebb7a37ca2d2d06998e4fc9ba1e9163c8b5884a
JUDY <judyyu00@students.cs.ubc.ca>
2019-09-11 14:05:25 -0700

    deliverable 1

---
+++ src/main/ui/ToDoList.java
@@ -0,0 +1,17 @@
+package ui;
+
+public class ToDoList {
+    public static void main(String[] args) {
+        shoppingList();
+        studyList();
+    }
+
+    public static void shoppingList() {
+        System.out.println("Grocery");
+    }
+
+    public static void studyList() {
+        System.out.println("Review");
+    }
+}
+
