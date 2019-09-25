958b1d526e28a30ed1675c360ed8d41c4f41f6a8
JUDY <judyyu00@students.cs.ubc.ca>
2019-09-25 10:31:30 -0700

    deliverable 3 completed, added more tests to TestToDoList

--- src/test/Test/TestToDoList.java
+++ src/test/Test/TestToDoList.java
@@ -9,23 +9,24 @@ import static org.junit.Assert.*;
 
 public class TestToDoList {
     ToDoList testToDoList;
-    Task myTask = new Task();
+    Task myTask;
     private model.Task Task;
 
     @Before
     public void setup(){
         testToDoList = new ToDoList();
+        myTask = new Task();
     }
 
     @Test
     public void testAddTaskNotThere() {
         //check that task is not already there
-        assertEquals(testToDoList.size(), 0);
+        assertEquals(0, testToDoList.size());
         assertTrue(testToDoList.doesNotContainTask("study"));
         //insert task into list
         testToDoList.addTask("study", myTask);
         //check that task is in list once
-        assertEquals(testToDoList.size(), 1);
+        assertEquals(1, testToDoList.size());
         assertFalse(testToDoList.doesNotContainTask("study"));
     }
 
@@ -39,6 +40,24 @@ public class TestToDoList {
     }
 
     @Test
+    public void testAddMultipleTasks() {
+        Task study = new Task();
+        Task work = new Task();
+        Task exercise = new Task();
+
+        assertEquals(0, testToDoList.size());
+        testToDoList.addTask("study", study);
+        testToDoList.addTask("work", work);
+        testToDoList.addTask("exercise", exercise);
+
+        assertEquals(3, testToDoList.size());
+        assertFalse(testToDoList.doesNotContainTask("study"));
+        assertFalse(testToDoList.doesNotContainTask("work"));
+        assertFalse(testToDoList.doesNotContainTask("exercise"));
+    }
+
+
+    @Test
     public void testRemoveTaskThere() {
         testToDoList.addTask("study", myTask);
         assertFalse(testToDoList.doesNotContainTask("study"));
@@ -46,36 +65,48 @@ public class TestToDoList {
         testToDoList.removeTask("study");
 
         assertTrue(testToDoList.doesNotContainTask("study"));
-        assertEquals(testToDoList.size(),0);
+        assertEquals(0, testToDoList.size());
     }
 
     @Test
     public void testRemoveTaskNotThere() {
         assertTrue(testToDoList.doesNotContainTask("study"));
-        assertEquals(testToDoList.size(),0);
+        assertEquals(0, testToDoList.size());
 
         testToDoList.removeTask("study");
 
         assertTrue(testToDoList.doesNotContainTask("study"));
-        assertEquals(testToDoList.size(),0);
+        assertEquals(0, testToDoList.size());
     }
 
     @Test
+    public void testRemoveWrongTask() {
+        testToDoList.addTask("study", myTask);
+        assertFalse(testToDoList.doesNotContainTask("study"));
+        assertEquals(1, testToDoList.size());
+
+        testToDoList.removeTask("work");
+        assertEquals(1, testToDoList.size());
+    }
+
+
+
+    @Test
     public void testDoesNotContainTask() {
-        assertEquals(testToDoList.size(), 0);
+        assertEquals(0, testToDoList.size());
         assertTrue(testToDoList.doesNotContainTask("study"));
     }
 
     @Test
     public void testContainTask() {
         testToDoList.addTask("study", myTask);
-        assertEquals(testToDoList.size(), 1);
+        assertEquals(1, testToDoList.size());
         assertFalse(testToDoList.doesNotContainTask("study"));
     }
 
     @Test
     public void testPrintEmptyTest() {
-        assertEquals(testToDoList.size(), 0);
+        assertEquals(0, testToDoList.size());
 //      TODO:how to do this?
     }
 }
