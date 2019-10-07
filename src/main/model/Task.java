c7a44d605c25d49f1d54802eaa291996495df27b
UBC Student <judyyu00@students.cs.ubc.ca>
2019-10-06 20:37:44 -0700

    Update Task.java

--- src/main/model/Task.java
+++ src/main/model/Task.java
@@ -1,26 +1,37 @@
 package model;
 
-public class Task {
-    String name;
 
-    public Task() {
-        name = "";
+public abstract class Task {
+    protected String category;
+    protected String course;
+    protected String name;
+    protected String type;
+
+    public Task(String name, String type) {
+        this.name = name;
+        this.type = type;
+
     }
 
     // getters
+    public String getType() {
+        return type;
+    }
+
     public String getName() {
         return name;
     }
 
     // setters
+    public void setType(String t) {
+        this.type = t;
+    }
+
     public void setName(String n) {
         this.name = n;
     }
 
-    // EFFECTS: print out the task's information
-    public void printTask() {
-        System.out.println(getName());
-    }
+    public abstract void printTask();
 
 }
 //
