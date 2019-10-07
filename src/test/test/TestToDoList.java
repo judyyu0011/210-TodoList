d08443d21fef685e2214d589147481fc20c07082
UBC Student <judyyu00@students.cs.ubc.ca>
2019-10-06 20:40:49 -0700

    Update TestToDoList.java

--- src/test/test/TestToDoList.java
+++ src/test/test/TestToDoList.java
@@ -1,5 +1,7 @@
 package test;
 
+import model.GeneralTask;
+import model.SchoolTask;
 import model.Task;
 import model.ToDoList;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,65 +11,80 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestToDoList {
     ToDoList testToDoList;
-    Task myTask;
+    Task myGeneralTask;
+    Task mySchoolTask;
 
     @BeforeEach
     public void setup(){
         testToDoList = new ToDoList();
-        myTask = new Task();
+        myGeneralTask = new GeneralTask("","", "");
+        mySchoolTask = new SchoolTask("","", "");
     }
 
     @Test
-    public void testAddTaskNotThere() throws FileNotFoundException, UnsupportedEncodingException {
+    public void testAddGeneralTask() {
         //check that task is not already there
         assertEquals(0, testToDoList.size());
-        assertTrue(testToDoList.doesNotContainTask("study"));
+        assertTrue(testToDoList.doesNotContainTask("run"));
         //insert task into list
-        testToDoList.addTask("study", myTask);
+        testToDoList.addGeneralTask("run", "exercise", "general", myGeneralTask);
         //check that task is in list once
         assertEquals(1, testToDoList.size());
-        assertFalse(testToDoList.doesNotContainTask("study"));
+        assertFalse(testToDoList.doesNotContainTask("run"));
     }
 
     @Test
-    public void testDuplicate() throws FileNotFoundException, UnsupportedEncodingException {
-        testToDoList.addTask("study", myTask);
-        testToDoList.addTask("study", myTask);
-
-        assertFalse(testToDoList.doesNotContainTask("study"));
+    public void testAddSchoolTask() throws FileNotFoundException, UnsupportedEncodingException {
+        //check that task is not already there
+        assertEquals(0, testToDoList.size());
+        assertTrue(testToDoList.doesNotContainTask("hw"));
+        //insert task into list
+        testToDoList.addSchoolTask("hw", "cpsc", "school", mySchoolTask);
+        //check that task is in list once
         assertEquals(1, testToDoList.size());
+        assertFalse(testToDoList.doesNotContainTask("hw"));
     }
+
+//    @Test
+//    public void testDuplicate() throws FileNotFoundException, UnsupportedEncodingException {
+//        testToDoList.addSchoolTask("essay", "school","ENGL112", mySchoolTask);
+//        testToDoList.addSchoolTask("essay", "school","ENGL112", mySchoolTask);
+//
+//        assertFalse(testToDoList.doesNotContainTask("essay"));
+//        assertEquals(1, testToDoList.size());
+//    }
 
     @Test
     public void testAddMultipleTasks() throws FileNotFoundException, UnsupportedEncodingException {
-        Task study = new Task();
-        Task work = new Task();
-        Task exercise = new Task();
+        Task essay = new SchoolTask("","", "");
+        Task eat = new GeneralTask("","", "");
+        Task exercise = new GeneralTask("","", "");
 
         assertEquals(0, testToDoList.size());
-        testToDoList.addTask("study", study);
-        testToDoList.addTask("work", work);
-        testToDoList.addTask("exercise", exercise);
+        testToDoList.addSchoolTask("essay", "school","ENGL112", essay);
+        testToDoList.addGeneralTask("eat", "general","everday", eat);
+        testToDoList.addGeneralTask("run", "general","exercise", exercise);
 
         assertEquals(3, testToDoList.size());
-        assertFalse(testToDoList.doesNotContainTask("study"));
-        assertFalse(testToDoList.doesNotContainTask("work"));
-        assertFalse(testToDoList.doesNotContainTask("exercise"));
+        assertFalse(testToDoList.doesNotContainTask("essay"));
+        assertFalse(testToDoList.doesNotContainTask("eat"));
+        assertFalse(testToDoList.doesNotContainTask("run"));
     }
 
 
     @Test
     public void testRemoveTaskThere() throws FileNotFoundException, UnsupportedEncodingException {
-        testToDoList.addTask("study", myTask);
-        assertFalse(testToDoList.doesNotContainTask("study"));
+        testToDoList.addGeneralTask("run", "exercise", "general", myGeneralTask);
+        assertFalse(testToDoList.doesNotContainTask("run"));
 
-        testToDoList.removeTask("study");
+        testToDoList.removeTask("run");
 
-        assertTrue(testToDoList.doesNotContainTask("study"));
+        assertTrue(testToDoList.doesNotContainTask("run"));
         assertEquals(0, testToDoList.size());
     }
 
@@ -84,12 +101,13 @@ public class TestToDoList {
 
     @Test
     public void testRemoveWrongTask() throws FileNotFoundException, UnsupportedEncodingException {
-        testToDoList.addTask("study", myTask);
-        assertFalse(testToDoList.doesNotContainTask("study"));
+        testToDoList.addSchoolTask("hw", "cpsc", "school", mySchoolTask);
+        assertFalse(testToDoList.doesNotContainTask("hw"));
         assertEquals(1, testToDoList.size());
 
         testToDoList.removeTask("work");
         assertEquals(1, testToDoList.size());
+        assertFalse(testToDoList.doesNotContainTask("hw"));
     }
 
     @Test
@@ -98,12 +116,12 @@ public class TestToDoList {
         assertTrue(testToDoList.doesNotContainTask("study"));
     }
 
-    @Test
-    public void testContainTask() throws FileNotFoundException, UnsupportedEncodingException {
-        testToDoList.addTask("study", myTask);
-        assertEquals(1, testToDoList.size());
-        assertFalse(testToDoList.doesNotContainTask("study"));
-    }
+//    @Test
+//    public void testContainTask() throws FileNotFoundException, UnsupportedEncodingException {
+//        testToDoList.addTask("study", myTask);
+//        assertEquals(1, testToDoList.size());
+//        assertFalse(testToDoList.doesNotContainTask("study"));
+//    }
 
     @Test
     public void testPrintEmptyTest() {
@@ -115,28 +133,40 @@ public class TestToDoList {
     public void testSaveAndLoad() throws IOException {
         ToDoList testSaveList = new ToDoList();
 
-        Task study = new Task();
-        Task hw = new Task();
-        Task work = new Task();
-        testToDoList.addTask("study", study);
-        testToDoList.addTask("hw", hw);
-        testToDoList.addTask("work", work);
+        Task essay = new SchoolTask("","", "");
+        Task eat = new GeneralTask("","", "");
+        Task exercise = new GeneralTask("","", "");
+        testToDoList.addSchoolTask("essay", "school","ENGL112", essay);
+        testToDoList.addGeneralTask("eat", "general","everday", eat);
+        testToDoList.addGeneralTask("run", "general","exercise", exercise);
 
         testToDoList.save("testfile1");
 
         testSaveList.load("testfile1");
 
-        assertFalse(testToDoList.doesNotContainTask("study"));
-        assertFalse(testToDoList.doesNotContainTask("hw"));
-        assertFalse(testToDoList.doesNotContainTask("work"));
+        assertFalse(testToDoList.doesNotContainTask("essay"));
+        assertFalse(testToDoList.doesNotContainTask("eat"));
+        assertFalse(testToDoList.doesNotContainTask("run"));
     }
 
     @Test
     public void testLoad() throws IOException {
         testToDoList.load("testfile");
-        assertFalse(testToDoList.doesNotContainTask("judy"));
-        assertFalse(testToDoList.doesNotContainTask("yu"));
+        assertFalse(testToDoList.doesNotContainTask("hw"));
+        assertFalse(testToDoList.doesNotContainTask("run"));
     }
+
+//    @Test
+//    public void testLoadNull() throws IOException {
+//        ToDoList testSaveList = new ToDoList();
+//        List<String> lines = new ArrayList<>();
+//
+//
+//        testSaveList.save("testfile2");
+//
+//        assertTrue(lines.contains("N/A"));
+//
+//    }
 }
 //
 
